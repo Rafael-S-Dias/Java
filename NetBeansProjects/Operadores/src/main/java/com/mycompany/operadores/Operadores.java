0cc0805336098cc9ab21af43843d9144486d5fe2
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-07-31 11:44:04 -0300

    Add files via upload

---
+++ NetBeansProjects/Operadores/src/main/java/com/mycompany/operadores/Operadores.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operadores;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Operadores {
+
+    public static void main(String[] args) {
+    // Variaveis
+    int a = 10;
+    int b = 2;
+    
+    int somar = a + b;
+    int subtrair = a - b;
+    int multiplicar = a * b;
+    int dividir = a / b;  
+
+    System.out.println("Somar: " +somar);
+    System.out.println("Subtrair: " +subtrair);
+    System.out.println("Multiplicar: " +multiplicar);
+    System.out.println("Dividir: " +dividir);
+    }
+}
---
+++ NetBeansProjects/Prestacoes/src/main/java/com/mycompany/prestacoes/Prestacoes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prestacoes;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Prestacoes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
