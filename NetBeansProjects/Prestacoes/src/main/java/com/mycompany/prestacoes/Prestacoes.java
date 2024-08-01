6f33a68c4bfce1c572b2fd651a2a6efb3f51d0bf
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-08-01 10:03:50 -0300

    Update Prestacoes.java

--- NetBeansProjects/Prestacoes/src/main/java/com/mycompany/prestacoes/Prestacoes.java
+++ NetBeansProjects/Prestacoes/src/main/java/com/mycompany/prestacoes/Prestacoes.java
@@ -12,21 +12,26 @@ import java.util.Scanner;
  */
 public class Prestacoes {
 
+    static int parcelas = 10;
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
                 
         System.out.println("Digite o preço do produto desejado: ");
         double produto = scan.nextDouble();
         
-        System.out.println("Digite em quantas parcelas desejada pagar: ");
-        int parcelas =  scan.nextInt();
-        
+        while (parcelas <0 || parcelas > 5) {
+            System.out.println("Digite em quantas parcelas desejada pagar em até 05 vezes sem juros: ");
+            parcelas = scan.nextInt();
+        }
         double valor = produto / parcelas;
         
+        System.out.println("Loja Mamão com Açucar");
         System.out.println("Seu pedido foi realizado com sucesso!");
         System.out.println("Valor total do produto: " + produto);
         System.out.println("Quantidade de parcelas: " + parcelas);
         System.out.println("Valor das parcelas: " + valor);
+        
         scan.close();
         
     }
