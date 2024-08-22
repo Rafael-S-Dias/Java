13dd6d67fa1050457c44aa6b5459c500415ce3ee
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-08-22 10:59:15 -0300

    Add files via upload

---
+++ NetBeansProjects/Ativiade6/Atividade6/src/main/java/com/mycompany/ativiade6/PrestacaoServico.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ativiade6;
+
+/**
+ *
+ * @author Aluno
+ */
+public class PrestacaoServico extends Juridica{
+    private String contratoInicio;
+    private String contratoFim;
+
+    public PrestacaoServico(String contratoInicio, String contratoFim, String cnpj, String inscricaoEstadual, String nome, String telefone, String email, Endereco endereco) {
+        super(cnpj, inscricaoEstadual, nome, telefone, email, endereco);
+        this.contratoInicio = contratoInicio;
+        this.contratoFim = contratoFim;
+    }
+
+    public String getContratoFim() {
+        return contratoFim;
+    }
+
+    public void setContratoFim(String contratoFim) {
+        this.contratoFim = contratoFim;
+    }
+
+    public String getContratoInicio() {
+        return contratoInicio;
+    }
+
+    public void setContratoInicio(String contratoInicio) {
+        this.contratoInicio = contratoInicio;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\nDados do Prestador de Serviço: " + 
+                super.toString() +
+                "\nInicio do Contrato: " + contratoInicio + 
+                "\nFim do Contrato: " + contratoFim;
+    }
+    
+}
