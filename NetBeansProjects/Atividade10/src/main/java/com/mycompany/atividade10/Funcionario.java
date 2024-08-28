0b0016523e1c56dae2dcc00f1982352f9e8872f0
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-08-28 11:44:53 -0300

    Add files via upload

---
+++ NetBeansProjects/Atividade10/src/main/java/com/mycompany/atividade10/Funcionario.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividade10;
+
+/**
+ *
+ * @author Aluno
+ */
+public abstract class Funcionario {
+    protected String nome;
+    protected String dataNascimento;
+    protected Sexo sexo;
+    protected Setor setor;
+    protected double salarioBase;
+
+    public Funcionario(String nome, String dataNascimento, Sexo sexo, Setor setor, double salarioBase) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.setor = setor;
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
+
+    public abstract double getSalarioFinal();
+            
+    @Override
+    public String toString() {
+        return "\nNome: " + nome + 
+                "\nData de Nascimento: " + dataNascimento + 
+                "\nSexo: " + sexo.getTexto() + 
+                "\nSetor: " + setor.getNome() +
+                "\nSalario Base: " + salarioBase;
+    }
+    
+    
+}
